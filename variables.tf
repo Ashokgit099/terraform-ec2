02fec58a7df7decf89472d7b5d1652bf611a8a21
Ashokgit099 <47042444+Ashokgit099@users.noreply.github.com>
2020-08-18 19:04:52 -0400

    This is the commit of terraform

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "ami_image"  {
+  default = "ami-b70554c8"
+}
+
+variable "name" {
+  description = "Name of the Instance"
+  default = "Instance1"
+}
+
+variable "security_groups" {
+  default = []
+}
+
+variable "subnets" {
+  default = "subnet-10c8635d"
+}
+
+variable "availability_zones" {
+  default = "us-east-1b"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "ashok2"
+}
+
+variable "vpc" {
+  default = "vpc-26e8dc5c"
+}
